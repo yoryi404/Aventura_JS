9707c38388df0715b29db138723bbd65dbeb457f
yoryi404 <jalegrem04@iesalbarregas.es>
2025-11-25 14:11:12 +0100

    Añadir funcionalidad de cambiar de escena

--- AventuraJS/js/main.js
+++ AventuraJS/js/main.js
@@ -1,18 +1,12 @@
-let cuchilla = new Producto("Cuchilla", "objects\Cuchilla.png", "1€", "Comun", "Arma", "1")
-let david = new Jugador("David", "characters\David.png")
-
-console.log(david.obtenerAtaqueTotal)
-
-
 import { showScene } from "./util.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-
     document.getElementById("btn-to-scene-2")
         .addEventListener("click", () => showScene("scene-2"));
 
     document.getElementById("btn-to-scene-3")
         .addEventListener("click", () => showScene("scene-3"));
 
+    document.getElementById("btn-to-scene-1")
+        .addEventListener("click", () => showScene("scene-1"));
 });
-
